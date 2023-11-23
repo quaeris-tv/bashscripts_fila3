caec2dfa559baa5623d6e832ee02015e1cdb33e8
marco76tv <marco.sottana@gmail.com>
2023-11-23 18:25:05 +0100

    Update git scripts and add directory information

--- git_branch.sh
+++ git_branch.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-git submodule foreach "git branch --set-upstream-to=origin/master master"
-git submodule foreach "git branch -m master"
-git submodule foreach "git merge master"
-git submodule foreach "git checkout master --"
 git branch --set-upstream-to=origin/master master
 git branch -m master
 git merge master
 git checkout master --
 #read -p "Press [Enter] key to exit..."
-echo '-------- END BRANCH ----------';
+echo "-------- END BRANCH[$(pwd)] ----------";
--- git_pull.sh
+++ git_pull.sh
@@ -1,7 +1,7 @@
 git submodule update --progress --init --recursive --force --merge --rebase --remote
-git submodule foreach git checkout master --
-git submodule foreach git pull origin master --autostash  --allow-unrelated-histories --prune --rebase
-git submodule foreach "git submodule foreach 'git pull origin master --rebase --autostash && git rebase origin/master && git merge master && git checkout master --'"
-git pull --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase  "origin"
+git checkout master --
+#git pull origin master --autostash  --allow-unrelated-histories --prune --rebase
+#git pull origin master --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase  "origin"
+git pull origin master --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
 #read -p "Press [Enter] key to exit..."
-echo '-------- END PULL ----------';
+echo "-------- END PULL[$(pwd)] ----------";
