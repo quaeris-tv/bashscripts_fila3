5eb74ee692cbf7719f3c0ad5d9b262f710836eca
marco76tv <marco.sottana@gmail.com>
2023-12-04 18:07:17 +0100

    .

--- git_init.sh
+++ git_init.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
-git submodule foreach "git config pull.rebase true"
-git submodule foreach "git remote set-branches --add origin master"
+git submodule foreach $( readlink -f -- "$0";)
 git config pull.rebase true
-git remote set-branches --add origin master
 git config remote.origin.push HEAD
-git submodule foreach git config remote.origin.push HEAD
-git config --global core.autocrlf true
-git config --global core.safecrlf false
-git config --global submodule.recurse true
-git config --global core.fileMode false
-git push --recurse-submodules=on-demand
-git config core.filemode false
 git config core.autocrlf true
-git submodule foreach --recursive git config core.filemode false
-git submodule foreach --recursive git config core.autocrlf true
-git submodule foreach git config advice.skippedCherryPicks false
+git config core.safecrlf false
+git config submodule.recurse true
+git config core.fileMode false
 git config advice.skippedCherryPicks false
+git remote set-branches --add origin master
+git push --recurse-submodules=on-demand
 git branch --set-upstream-to=origin/master master
-git submodule foreach git branch --set-upstream-to=origin/master master
+
