5c07603c832676544b26475cc683278450011e5c
marco76tv <marco.sottana@gmail.com>
2023-12-06 15:42:12 +0100

    Update composer dependencies and add larastan/larastan

--- composer_global.sh
+++ composer_global.sh
@@ -6,10 +6,10 @@ php -r "unlink('composer.lock');"
 rm composer.lock
 rm package-lock.json
 
-#cs fixer 
+#cs fixer
 php -d memory_limit=-1 composer.phar global require -W friendsofphp/php-cs-fixer
 #grumphp
 php -d memory_limit=-1 composer.phar global require -W phpro/grumphp
 #phpstan
-php -d memory_limit=-1 composer.phar global require -W --dev nunomaduro/larastan
+php -d memory_limit=-1 composer.phar global require -W --dev larastan/larastan
 php -d memory_limit=-1 composer.phar global require -W --dev phpstan/phpstan
