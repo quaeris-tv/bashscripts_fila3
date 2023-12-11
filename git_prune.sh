2893209daab9185e40497f38ede67f874c060e25
marco76tv <marco.sottana@gmail.com>
2023-12-11 12:29:36 +0100

    Optimize git pruning and garbage collection

--- git_prune.sh
+++ git_prune.sh
@@ -1,13 +1,12 @@
 git submodule foreach $( readlink -f -- "$0"; );
 git fsck --full --unreachable
 git filter-branch -- --all
-git gc --auto --aggressive
-git gc --aggressive --prune=now --force
 git reflog expire --all --expire=now
 git pack-refs --all --prune
-git gc --prune=now --aggressive
 git repack -A -d
 git prune
+git gc --auto --aggressive
+git gc --aggressive --prune=now --force
 #read -p "Press [Enter] key to exit..."
 echo "-------- END PRUNE[$(pwd)] ----------";
 
