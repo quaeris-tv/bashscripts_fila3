c4983891633dfcde26d955924e766a847e8b7136
marco76tv <marco.sottana@gmail.com>
2023-12-20 13:34:48 +0100

    up

--- git_delete_history_dev_recursive.sh
+++ git_delete_history_dev_recursive.sh
@@ -6,6 +6,10 @@ git commit -am "first"
 git branch -D dev  # Deletes the dev branch
 git branch -m dev  # Rename the current branch to dev
 git gc --aggressive --prune=all     # remove the old files
+<<<<<<< HEAD
+git push -uf origin dev  # Force push dev branch to github
+=======
 git push -u origin dev  # Force push dev branch to github
 git pull origin dev 
+>>>>>>> dev
 git gc --aggressive --prune=all     # remove the old files
