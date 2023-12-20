5f13fe2ebdf40e5a462eb20563482001776b256f
marco xot <marco.sottana@gmail.com>
2023-12-20 13:15:18 +0100

    first

---
+++ curl/passport02.php
@@ -0,0 +1,9 @@
+https://{yourDomain}/authorize?
+    response_type=code&
+    code_challenge={codeChallenge}&
+    code_challenge_method=S256&
+    client_id={yourClientId}&
+    redirect_uri={yourCallbackUrl}&
+    scope=SCOPE&
+    audience={apiAudience}&
+    state={state}
