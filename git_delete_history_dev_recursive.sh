1283aaae8423444c85d2a864d1c3e57661ac7a3a
marco76tv <marco.sottana@gmail.com>
2023-12-20 11:33:10 +0100

    first

---
+++ git_delete_history_dev_recursive.sh
@@ -0,0 +1,10 @@
+git submodule foreach $( readlink -f -- "$0";)
+git checkout --orphan newBranch
+git add --renormalize -A
+git add -A  # Add all files and commit them
+git commit -am "first"
+git branch -D dev  # Deletes the dev branch
+git branch -m dev  # Rename the current branch to dev
+git gc --aggressive --prune=all     # remove the old files
+git push -u origin dev  # Force push dev branch to github
+git gc --aggressive --prune=all     # remove the old files
