53e93d4843fe60b69724d947146394477f10c867
marco76tv <marco.sottana@gmail.com>
2023-12-22 14:15:37 +0000

    up

--- composer_init.sh
+++ composer_init.sh
@@ -7,34 +7,34 @@ rm composer.lock
 rm package-lock.json
 
 ################# REMOVE ##################
-php -d memory_limit=-1 composer.phar remove -W --ignore-platform-reqs laravel/sanctum
+php -d memory_limit=-1 composer.phar remove -W  laravel/sanctum
 
 
 ############## priority 1 ####################
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs filament/filament:"^3.1"
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs coolsam/modules
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs konnco/filament-import
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs thecodingmachine/safe
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel/passport
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs socialiteproviders/auth0
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs jenssegers/agent
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs owenvoke/blade-fontawesome
+php -d memory_limit=-1 composer.phar require -W  filament/filament:"^3.1"
+php -d memory_limit=-1 composer.phar require -W  coolsam/modules
+php -d memory_limit=-1 composer.phar require -W  konnco/filament-import
+php -d memory_limit=-1 composer.phar require -W  thecodingmachine/safe
+php -d memory_limit=-1 composer.phar require -W  laravel/passport
+php -d memory_limit=-1 composer.phar require -W  socialiteproviders/auth0
+php -d memory_limit=-1 composer.phar require -W  jenssegers/agent
+php -d memory_limit=-1 composer.phar require -W  owenvoke/blade-fontawesome
 php artisan icons:cache
 
 ############### SPATIE #######################
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-cookie-consent
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-permission
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-queueable-action
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-data
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-tags
-php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-event-sourcing
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-cookie-consent
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-permission
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-queueable-action
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-data
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-tags
+php -d memory_limit=-1 composer.phar require -W  spatie/laravel-event-sourcing
 
 
 ########### dev #######################
-php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs thecodingmachine/phpstan-safe-rule
-php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs barryvdh/laravel-debugbar
-php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs barryvdh/laravel-ide-helper
-php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs larastan/larastan
-php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs orchestra/testbench
+php -d memory_limit=-1 composer.phar require -W --dev  thecodingmachine/phpstan-safe-rule
+php -d memory_limit=-1 composer.phar require -W --dev  barryvdh/laravel-debugbar
+php -d memory_limit=-1 composer.phar require -W --dev  barryvdh/laravel-ide-helper
+php -d memory_limit=-1 composer.phar require -W --dev  larastan/larastan
+php -d memory_limit=-1 composer.phar require -W --dev  orchestra/testbench
 
 
