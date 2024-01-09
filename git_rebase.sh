8f35797d900cab8145b533f1ba9a8cc9b4038d0c
marco76tv <marco.sottana@gmail.com>
2024-01-09 18:06:42 +0100

    up

--- git_rebase.sh
+++ git_rebase.sh
@@ -1,64 +1,6 @@
 echo "N: $1"
 for(( i=1; i<=$1; i++ ))
 do
-<<<<<<< HEAD
-<<<<<<< HEAD
 #git add -A && git commit -am "rebase $1" && git rebase --continue || git rebase --continue || git push -uf || echo "loop: $i"
 git add -A && aicommits && git rebase --continue || git rebase --continue || git push -uf || echo "loop: $i"
-<<<<<<< HEAD
-<<<<<<< HEAD
 done 
-=======
-done 
->>>>>>> 1283aaa (first)
-=======
-done 
->>>>>>> dd31420 (first)
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-=======
-git add -A && git commit -am "rebase" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> a176205 (first)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> b7048e3 (Update git branch and rebase scripts)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> a6fcef9 (Update git branch and rebase scripts)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> dev
-=======
-git add -A && git commit -am "rebase" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> a176205 (first)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> b7048e3 (Update git branch and rebase scripts)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> a6fcef9 (Update git branch and rebase scripts)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
->>>>>>> b3a67b2 (first)
-done 
->>>>>>> c219998 (first)
-=======
-#git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-done 
->>>>>>> b3a67b2 (first)
