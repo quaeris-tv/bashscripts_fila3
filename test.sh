2fadd7aaebddae2851a92ca861085632bb5d6bc3
marco76tv <marco.sottana@gmail.com>
2024-01-17 18:29:46 +0100

    .

--- test.sh
+++
@@ -1,9 +0,0 @@
-#git submodule foreach $(pwd -P)/bashscripts/test.sh
-#git submodule foreach $( dirname -- "${BASH_SOURCE[0]}" )/test.sh
-git submodule foreach $( readlink -f -- "$0";)
-#ls
-#echo "The script you are running has:"
-#echo "basename: [$(basename "$0")]"
-#echo "dirname : [$(dirname "$0")]"
-echo "pwd     : [$(pwd)]"
-#echo " :[$dirname -- "$( readlink -f -- "$0"; )"";
--- test01.sh
+++
@@ -1,2 +0,0 @@
-git submodule foreach $( readlink -f -- "$0"; );
-ls
