36ccc47dcefaf47b6cf116c394b10b6bd0f22d29
marco76tv <marco.sottana@gmail.com>
2024-01-17 18:28:19 +0100

    rebase 25

--- git_delete_history_master.sh
+++ git_delete_history_master.sh
@@ -1,4 +1,5 @@
 git checkout --orphan newBranch
+<<<<<<< HEAD
 git add --renormalize -A
 git add -A  # Add all files and commit them
 git commit -am "first"
@@ -7,3 +8,13 @@ git branch -m master  # Rename the current branch to master
 git gc --aggressive --prune=all     # remove the old files
 git push -uf origin master  # Force push master branch to github
 git gc --aggressive --prune=all     # remove the old files
+=======
+git add -A  # Add all files and commit them
+git commit -am .
+git branch -D master  # Deletes the master branch
+git branch -m master  # Rename the current branch to master
+git gc --aggressive --prune=all     # remove the old files
+git push -f origin master  # Force push master branch to github
+git gc --aggressive --prune=all     # remove the old files
+git gc --auto
+>>>>>>> c516d6a (rebase 1)
