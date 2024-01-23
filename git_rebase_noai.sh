50f53fbb60bcb2b8f40348efb68c96d111058cab
marco76tv <marco.sottana@gmail.com>
2024-01-23 15:47:48 +0100

    .

--- git_rebase_noai.sh
+++
@@ -1,6 +0,0 @@
-echo "N: $1"
-for(( i=1; i<=$1; i++ ))
-do
-git add -A && git commit -am "rebase $i" && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-#git add -A && aicommits && git rebase --continue || git rebase --continue || git push -u --force || echo "loop: $i"
-done 
