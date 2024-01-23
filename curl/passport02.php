29d70edc8dec48881a193ccfa8b78167587c02d3
marco76tv <marco.sottana@gmail.com>
2024-01-23 08:46:43 +0100

    up

--- curl/passport02.php
+++
@@ -1,9 +0,0 @@
-https://{yourDomain}/authorize?
-    response_type=code&
-    code_challenge={codeChallenge}&
-    code_challenge_method=S256&
-    client_id={yourClientId}&
-    redirect_uri={yourCallbackUrl}&
-    scope=SCOPE&
-    audience={apiAudience}&
-    state={state}
--- git_delete_histories_dev.sh
+++
@@ -1,9 +0,0 @@
-git checkout --orphan newBranch
-git add -A  # Add all files and commit them
-git commit -am .
-git branch -D dev  # Deletes the dev branch
-git branch -m dev  # Rename the current branch to dev
-git gc --aggressive --prune=all     # remove the old files
-git push -f origin dev  # Force push dev branch to github
-git gc --aggressive --prune=all     # remove the old files
-git gc --auto
