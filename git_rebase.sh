17bcaa8deaf0544088eae544807dff237422c76b
marco76tv <marco.sottana@gmail.com>
2024-02-27 10:28:16 +0100

    chore(git_rebase.sh): update git rebase script to use 'oco' alias instead of 'aicommits' for better clarity and consistency

--- git_rebase.sh
+++ git_rebase.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 echo "N: $1"
 for(( i=1; i<=$1; i++ ))
 do
 #git add -A && git commit -am "rebase $1" && git rebase --continue || git rebase --continue || git push -uf || echo "loop: $i"
-git add -A && aicommits && git rebase --continue || git rebase --continue || git push -uf || echo "loop: $i"
+#git add -A && aicommits && git rebase --continue || git rebase --continue || git push -uf || echo "loop: $i"
+git add -A && oco && git rebase --continue || git rebase --continue || git push -u || echo "loop: $i"
 done 
