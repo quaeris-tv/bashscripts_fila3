8664bf5795c1f4b823e475e00c1cef1c8765b2ea
marco76tv <marco.sottana@gmail.com>
2024-02-27 09:33:10 +0100

    feat(git_up_oco.sh): add script for updating all submodules and performing necessary git operations
    The script `git_up_oco.sh` is added to the repository. This script is used to update all submodules and perform necessary git operations.
    
    The script takes a branch name as an argument and performs the following steps:
    1. Retrieves the absolute path of the script and stores it in the `me` variable.
    2. Stores the branch name in the `branch` variable.
    3. Retrieves the current working directory and stores it in the `where` variable.
    4. Executes the script recursively for each submodule using the `git submodule foreach` command, passing the `me` variable and the `branch` variable as arguments.
    5. Prints a start message indicating the start of the script execution, including the current working directory and the branch name.
    6. Executes the `oco` command or prints "--- no changes" if the command fails.
    7. Prints an end message indicating the end of the push operation, including the current working directory and the branch name.
    8. Checks out the specified branch using the `git checkout` command.
    9. Merges the specified branch using the `git merge` command.
    10. Prints an end message indicating the end of the branch operation, including the current working directory and the branch name.
    11. Pulls the specified branch from the origin repository using the `git pull` command, with additional options for autostashing, allowing unrelated histories, pruning, and showing progress.
    12. Prints an end message indicating the end of the pull operation, including the current working directory and the branch name.
    13. Displays the current status of the repository using the `git status` command.

--- git_up_oco.sh
+++ git_up_oco.sh
@@ -6,7 +6,11 @@ git submodule foreach "$me" "$branch"
 echo "-------- START[$where ($branch)] ----------";
 oco || echo '--- no changes'
 echo "-------- END PUSH[$where ($branch)] ----------";
+<<<<<<< HEAD
 git checkout $branch -- || git branch $branch && git checkout $branch
+=======
+git checkout $branch --
+>>>>>>> feat(git_up_oco.sh): add script for updating all submodules and performing necessary git operations
 git merge $branch
 echo "-------- END BRANCH[$where ($branch)] ----------";
 git pull origin $branch --autostash --allow-unrelated-histories --prune --progress -v 
