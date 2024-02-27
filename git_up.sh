dd0e9ce206690bd18f8ec174f055700832229115
marco76tv <marco.sottana@gmail.com>
2024-02-27 09:17:24 +0100

    Refactor script, update git commands, add status check

--- git_up.sh
+++ git_up.sh
@@ -1,16 +1,21 @@
-git submodule foreach $( readlink -f -- "$0";)
+#!/bin/sh
+me=$( readlink -f -- "$0";)
+branch=$1
+git submodule foreach "$me" "$branch"
+echo "-------- START[$(pwd) ($branch)] ----------";
 git add --renormalize -A
 git add -A && aicommits  || echo '---------------------------empty'
-git push origin $1 -u --progress 'origin' || git push --set-upstream origin $1
-echo "-------- END PUSH[$(pwd) ($1)] ----------";
-git checkout $1 --
-git branch --set-upstream-to=origin/$1 $1
-git branch -u origin/$1
-git merge $1
-echo "-------- END BRANCH[$(pwd) ($1)] ----------";
+git push origin $branch -u --progress 'origin' || git push --set-upstream origin $branch
+echo "-------- END PUSH[$(pwd) ($branch)] ----------";
+git checkout $branch --
+git branch --set-upstream-to=origin/$branch $branch
+git branch -u origin/$branch
+git merge $branch
+echo "-------- END BRANCH[$(pwd) ($branch)] ----------";
 git submodule update --progress --init --recursive --force --merge --rebase --remote
-git checkout $1 --
-git pull origin $1 --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
-echo "-------- END PULL[$(pwd) ($1)] ----------";
+git checkout $branch --
+git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
+echo "-------- END PULL[$(pwd) ($branch)] ----------";
+git status
 
 
