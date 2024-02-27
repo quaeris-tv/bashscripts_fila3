465c8a30719254d5ac459c5382bf4156003b862a
marco76tv <marco.sottana@gmail.com>
2024-02-27 15:38:02 +0100

    conflict

--- git_up_oco.sh
+++ git_up_oco.sh
@@ -6,15 +6,7 @@ git submodule foreach "$me" "$branch"
 echo "-------- START[$where ($branch)] ----------";
 oco || echo '--- no changes'
 echo "-------- END PUSH[$where ($branch)] ----------";
-<<<<<<< HEAD
-<<<<<<< HEAD
 git checkout $branch -- || git branch $branch && git checkout $branch
-=======
-git checkout $branch --
->>>>>>> feat(git_up_oco.sh): add script for updating all submodules and performing necessary git operations
-=======
-git checkout $branch -- || git branch $branch && git checkout $branch
->>>>>>> fix(git_up_oco.sh): handle case where branch does not exist locally by creating and checking out the branch
 git merge $branch
 echo "-------- END BRANCH[$where ($branch)] ----------";
 git pull origin $branch --autostash --allow-unrelated-histories --prune --progress -v 
