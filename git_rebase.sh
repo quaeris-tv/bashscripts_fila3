06c9e52037f14fd8fcc996cb712f936c3f0e668a
marco76tv <marco.sottana@gmail.com>
2024-04-15 09:51:46 +0200

    up

--- git_rebase.sh
+++ git_rebase.sh
@@ -1,9 +1,16 @@
-<<<<<<< HEAD
 #!/bin/bash
-=======
->>>>>>> master
-echo "N: $1"
-for(( i=1; i<=$1; i++ ))
+if [ "$2" ]; then
+     echo yes
+else
+    echo 'aggiungere il branch ./bashscripts/git_rebase.sh  <count> <branch>'
+    exit 1
+fi
+me=$( readlink -f -- "$0";)
+count=$1
+branch=$2
+where=$(pwd)
+echo "N: $count"
+for(( i=1; i<=$count; i++ ))
 do
-git add -A && oco && git rebase --continue || git rebase --continue || git push -u || echo "loop: $i"
+git add -A && oco && git rebase --continue || git push -uf origin HEAD:$branch && git rebase --continue || git rebase --continue || echo "loop: $i"
 done 
