eee049af55f772bdde25d810ecb430b17d4f4c73
marco76tv <marco.sottana@gmail.com>
2024-04-16 12:26:09 +0200

    .

--- git_rebase.sh
+++ git_rebase.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+<<<<<<< HEAD
 if [ "$2" ]; then
      echo yes
 else
@@ -13,4 +14,10 @@ echo "N: $count"
 for(( i=1; i<=$count; i++ ))
 do
 git add -A && oco && git rebase --continue || git push -uf origin HEAD:$branch && git rebase --continue || git rebase --continue || echo "loop: $i"
+=======
+echo "N: $1"
+for(( i=1; i<=$1; i++ ))
+do
+git add -A && oco && git rebase --continue || git rebase --continue || git push -u || echo "loop: $i"
+>>>>>>> e167c2c6 (up)
 done 
