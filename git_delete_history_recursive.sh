bef48435c9ad94fe46f5d3e5497052e6d7395c8d
marco76tv <marco.sottana@gmail.com>
2024-04-17 09:21:37 +0200

    .

--- git_delete_history_recursive.sh
+++ git_delete_history_recursive.sh
@@ -1,11 +1,22 @@
-git submodule foreach $( readlink -f -- "$0";)
-git checkout --orphan newBranch
+#!/bin/sh
+if [ "$1" ]; then
+     echo yes
+else
+    echo 'aggiungere il branch ./bashscripts/git_delete_history_recursive.sh  <branch>'
+    exit 1
+fi
+me=$( readlink -f -- "$0";)
+branch=$1
+where=$(pwd)
+git submodule foreach "$me" "$branch"
+git checkout --orphan newBranch$branch
 git add --renormalize -A
 git add -A  # Add all files and commit them
 git commit -am "first"
-git branch -D $1  # Deletes the $1 branch
-git branch -m $1  # Rename the current branch to $1
+git branch -D $branch  # Deletes the $1 branch
+git branch -m $branch  # Rename the current branch to $1
 git gc --aggressive --prune=all     # remove the old files
-git push -uf origin $1  # Force push $1 branch to github
+git push -uf origin $branch  # Force push $1 branch to github
 git gc --aggressive --prune=all     # remove the old files
 git gc --auto
+echo "-------- END [$where ($branch)] ----------";
