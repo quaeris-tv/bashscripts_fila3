20a76851b90302a86f4c37d69671620d0d709174
marco76tv <marco.sottana@gmail.com>
2024-04-24 13:59:44 +0200

    .

--- git_rebase_noai.sh
+++ git_rebase_noai.sh
@@ -12,10 +12,5 @@ number=$1
 echo "N: $number"
 for(( i=1; i<=$number; i++ ))
 do
-<<<<<<< HEAD
 git add -A && git commit -am "rebase $1" && git push origin HEAD:$branch -uf && git rebase --continue || git rebase --continue || git push origin HEAD:$branch -uf || echo "loop: $i"
 done 
-=======
-git add -A && git commit -am "rebase $1" && git push origin HEAD:dev -u && git rebase --continue || git rebase --continue || git push origin HEAD:dev -uf || echo "loop: $i"
-done 
->>>>>>> d95479d99794b50c6095c5f6ddf1f05a877b0c9e
