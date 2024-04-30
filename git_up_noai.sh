098fb78a8c042749498becf19483a15f6d60d9cb
marco76tv <marco.sottana@gmail.com>
2024-04-30 16:07:05 +0200

    .

--- git_up_noai.sh
+++ git_up_noai.sh
@@ -12,16 +12,16 @@ git submodule foreach "$me" "$branch"
 git add --renormalize -A
 #git add -A && aicommits  || echo '---------------------------empty'
 git add -A && git commit -am "up"  || echo '---------------------------empty'
-git push origin $1 -u --progress 'origin' || git push --set-upstream origin $1
-echo "-------- END PUSH[$where ($1)] ----------";
-git checkout $1 --
-git branch --set-upstream-to=origin/$1 $1
-git branch -u origin/$1
-git merge $1
-echo "-------- END BRANCH[$where ($1)] ----------";
+git push origin $branch -u --progress 'origin' || git push --set-upstream origin $branch
+echo "-------- END PUSH[$where ($branch)] ----------";
+git checkout $branch --
+git branch --set-upstream-to=origin/$branch $branch
+git branch -u origin/$branch
+git merge $branch
+echo "-------- END BRANCH[$where ($branch)] ----------";
 git submodule update --progress --init --recursive --force --merge --rebase --remote
-git checkout $1 --
-git pull origin $1 --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
-echo "-------- END PULL[$where ($1)] ----------";
+git checkout $branch --
+git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
+echo "-------- END PULL[$where ($branch)] ----------";
 
 
