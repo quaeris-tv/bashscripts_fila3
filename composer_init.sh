8ab6b73e48597855c402497912c9d4c09f9152f4
marco76tv <marco.sottana@gmail.com>
2024-06-24 09:10:38 +0200

    up

--- composer_init.sh
+++ composer_init.sh
@@ -8,79 +8,79 @@ rm package-lock.json
 #mv composer.json composer_$(date +"%Y-%m-%d").json
 #php composer.phar init
 
-php -d memory_limit=-1 composer.phar require -W filament/filament
-php -d memory_limit=-1 composer.phar require -W coolsam/modules
-php -d memory_limit=-1 composer.phar require -W nwidart/laravel-modules
-php -d memory_limit=-1 composer.phar require -W doctrine/dbal
-php -d memory_limit=-1 composer.phar require -W livewire/livewire
-php -d memory_limit=-1 composer.phar require -W livewire/volt
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs filament/filament
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs coolsam/modules
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs nwidart/laravel-modules
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs doctrine/dbal
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs livewire/livewire
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs livewire/volt
 ### SPATIE
-php -d memory_limit=-1 composer.phar require -W filament/spatie-laravel-tags-plugin
-php -d memory_limit=-1 composer.phar require -W filament/spatie-laravel-media-library-plugin
-php -d memory_limit=-1 composer.phar require -W filament/spatie-laravel-translatable-plugin
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-cookie-consent
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-data
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-event-sourcing
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-permission
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-queueable-action
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-model-status
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-model-states
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-query-builder
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-activitylog
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-schemaless-attributes
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-feed
-php -d memory_limit=-1 composer.phar require -W spatie/laravel-health
-php -d memory_limit=-1 composer.phar require -W spatie/security-advisories-health-check
-php -d memory_limit=-1 composer.phar require -W spatie/cpu-load-health-check
-php -d memory_limit=-1 composer.phar require -W spatie/crawler
-php -d memory_limit=-1 composer.phar require -W spatie/url
-php -d memory_limit=-1 composer.phar require -W spatie/color
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs filament/spatie-laravel-tags-plugin
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs filament/spatie-laravel-media-library-plugin
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs filament/spatie-laravel-translatable-plugin
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-cookie-consent
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-data
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-event-sourcing
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-permission
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-queueable-action
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-model-status
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-model-states
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-query-builder
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-activitylog
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-schemaless-attributes
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-feed
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/laravel-health
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/security-advisories-health-check
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/cpu-load-health-check
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/crawler
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/url
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/color
 ### USER 
-php -d memory_limit=-1 composer.phar require -W jenssegers/agent
-php -d memory_limit=-1 composer.phar require -W laravel/passport
-php -d memory_limit=-1 composer.phar require -W socialiteproviders/auth0
-php -d memory_limit=-1 composer.phar require -W tightenco/parental
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs jenssegers/agent
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel/passport
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs socialiteproviders/auth0
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs tightenco/parental
 ### NOTIFY
-php -d memory_limit=-1 composer.phar require -W kreait/firebase-php
-php -d memory_limit=-1 composer.phar require -W laravel-notification-channels/telegram
-php -d memory_limit=-1 composer.phar require -W laravel/slack-notification-channel
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs kreait/firebase-php
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel-notification-channels/telegram
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel/slack-notification-channel
 ### MEDIA
-php -d memory_limit=-1 composer.phar require -W pbmedia/laravel-ffmpeg
-php -d memory_limit=-1 composer.phar require -W intervention/image
-php -d memory_limit=-1 composer.phar require -W spatie/image
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs pbmedia/laravel-ffmpeg
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs intervention/image
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spatie/image
 ### PROFILE 
-php -d memory_limit=-1 composer.phar require -W tightenco/parental
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs tightenco/parental
 ### UI
-php -d memory_limit=-1 composer.phar require -W mhmiton/laravel-modules-livewire
-php -d memory_limit=-1 composer.phar require -W laravel/breeze
-php -d memory_limit=-1 composer.phar require -W owenvoke/blade-fontawesome
-php -d memory_limit=-1 composer.phar require -W laravel/folio
-php -d memory_limit=-1 composer.phar require -W cknow/laravel-money
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs mhmiton/laravel-modules-livewire
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel/breeze
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs owenvoke/blade-fontawesome
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs laravel/folio
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs cknow/laravel-money
 php artisan folio:install
-php -d memory_limit=-1 composer.phar require -W livewire/volt
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs livewire/volt
 php artisan volt:install
 #php -d memory_limit=-1 composer.phar require -W guava/filament-icon-picker
 ## IMPORT/EXPORT
 #php -d memory_limit=-1 composer.phar require -W konnco/filament-import
-php -d memory_limit=-1 composer.phar require -W spipu/html2pdf
-php -d memory_limit=-1 composer.phar require -W maatwebsite/excel
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs spipu/html2pdf
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs maatwebsite/excel
 ####
-php -d memory_limit=-1 composer.phar require -W thecodingmachine/safe
-php -d memory_limit=-1 composer.phar require -W symfony/dom-crawler
-php -d memory_limit=-1 composer.phar require -W flowframe/laravel-trend
-php -d memory_limit=-1 composer.phar require -W staudenmeir/laravel-adjacency-list
-php -d memory_limit=-1 composer.phar require -W fidum/laravel-eloquent-morph-to-one
-php -d memory_limit=-1 composer.phar require -W calebporzio/sushi
-php -d memory_limit=-1 composer.phar require -W predis/predis
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs thecodingmachine/safe
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs symfony/dom-crawler
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs flowframe/laravel-trend
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs staudenmeir/laravel-adjacency-list
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs fidum/laravel-eloquent-morph-to-one
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs calebporzio/sushi
+php -d memory_limit=-1 composer.phar require -W --ignore-platform-reqs predis/predis
 
 ### DEV
-php -d memory_limit=-1 composer.phar require -W --dev barryvdh/laravel-debugbar
-php -d memory_limit=-1 composer.phar require -W --dev barryvdh/laravel-ide-helper
-php -d memory_limit=-1 composer.phar require -W --dev thecodingmachine/phpstan-safe-rule
-php -d memory_limit=-1 composer.phar require -W --dev larastan/larastan
-php -d memory_limit=-1 composer.phar require -W --dev laravel/pint
-php -d memory_limit=-1 composer.phar require -W --dev pestphp/pest
-php -d memory_limit=-1 composer.phar require -W --dev pestphp/pest-plugin-laravel
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs barryvdh/laravel-debugbar
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs barryvdh/laravel-ide-helper
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs thecodingmachine/phpstan-safe-rule
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs larastan/larastan
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs laravel/pint
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs pestphp/pest
+php -d memory_limit=-1 composer.phar require -W --dev --ignore-platform-reqs pestphp/pest-plugin-laravel
 
 ### REMOVE
 php -d memory_limit=-1 composer.phar remove laravel/sanctum
