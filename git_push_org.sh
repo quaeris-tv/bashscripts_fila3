919bb03f9b2161a9379c69669e5542d3b9845a67
marco76tv <marco.sottana@gmail.com>
2024-11-29 15:53:54 +0100

    up

--- git_push_org.sh
+++ git_push_org.sh
@@ -42,24 +42,15 @@ while IFS= read -r line; do
             REPO_NAME=$(basename "$(git config --get remote.origin.url)" .git)
             
             # Create new remote URL
-            NEW_REMOTE="https://github.com/$NEW_ORG/$REPO_NAME.git"
+            #NEW_REMOTE="https://github.com/$NEW_ORG/$REPO_NAME.git"
+            NEW_REMOTE="git@github.com:$NEW_ORG/$REPO_NAME.git"
             
             echo "Pushing to: $NEW_REMOTE"
             
             # Optional: Fetch from the new remote and merge (uncomment if needed)
             #git fetch "$NEW_REMOTE"
             #git merge "$NEW_REMOTE/$(git rev-parse --abbrev-ref HEAD)" || echo "Failed to merge changes for $SUBMODULE_PATH"
-<<<<<<< HEAD
-            
-            # Push the submodule to the new remote
-            git push -u "$NEW_REMOTE" || {
-                echo "Error: Failed to push submodule $SUBMODULE_PATH to $NEW_REMOTE"
-                exit 1
-            }
-
-=======
-            git push -u "$NEW_REMOTE" HEAD:dev
->>>>>>> origin/dev
+            git push -uf "$NEW_REMOTE" HEAD:dev
             echo "----------------------------------------"
         )
     fi
