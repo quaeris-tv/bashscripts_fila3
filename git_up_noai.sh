d92c64f038d6b7cbab6d441e1d6664ddeb1598c9
marco76tv <marco.sottana@gmail.com>
2025-02-26 19:08:56 +0100

    up

--- git_up_noai.sh
+++ git_up_noai.sh
@@ -11,6 +11,16 @@ where=$(pwd)
 git submodule update --progress --init --recursive --force --merge --rebase --remote
 git submodule foreach "$me" "$branch"
 git config core.fileMode false
+#old branches
+git push origin --delete cs0.2.03
+git push origin --delete cs0.2.04
+git push origin --delete cs0.2.05
+git push origin --delete cs0.2.06
+git push origin --delete cs0.2.07
+git push origin --delete cs0.2.08
+git push origin --delete cs0.2.09
+git push origin --delete cs0.2.10
+#
 git add --renormalize -A
 #git add -A && aicommits  || echo '---------------------------empty'
 git add -A && git commit -am "up"  || echo '---------------------------empty'
@@ -24,15 +34,6 @@ echo "-------- END BRANCH[$where ($branch)] ----------";
 git submodule update --progress --init --recursive --force --merge --rebase --remote
 git checkout $branch --
 git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
-#old branches
-#git push origin --delete cs0.2.03
-#git push origin --delete cs0.2.04
-#git push origin --delete cs0.2.05
-#git push origin --delete cs0.2.06
-#git push origin --delete cs0.2.07
-#git push origin --delete cs0.2.08
-#git push origin --delete cs0.2.09
-#git push origin --delete cs0.2.10
 sed -i -e 's/\r$//' "$me"
 echo "-------- END PULL[$where ($branch)] ----------";
 
