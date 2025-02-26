19fa84194799ddf2ce6866455a298bfce0300a75
marco76tv <marco.sottana@gmail.com>
2025-02-26 18:33:41 +0100

    .

--- sync_to_disk.sh
+++ sync_to_disk.sh
@@ -8,9 +8,10 @@ fi
 
 DISK_NAME=$1
 DEST_PATH="/mnt/$DISK_NAME$PWD"
+me=$( readlink -f -- "$0";)
 
 echo "Sincronizzazione in corso da '$PWD' a '$DEST_PATH'..."
 
-rsync -avz --relative --exclude='.git' --exclude='build' --exclude='cache'  --exclude='storage' --exclude='venv' --exclude='node_modules' --exclude='vendor' ./ "$DEST_PATH"
-
+rsync -avz --relative --exclude='.git' --exclude='build' --exclude='cache'  --exclude='storage' --exclude='venv' --exclude='node_modules' --exclude='vendor' --exclude='stubs' ./ "$DEST_PATH"
+sed -i -e 's/\r$//' "$me"
 echo "Sincronizzazione completata!"
