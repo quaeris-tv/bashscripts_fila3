8d408187f320073a90f3636893f5327052eed199
marco76tv <marco.sottana@gmail.com>
2025-02-26 10:40:00 +0100

    up

--- git_up_noai.sh
+++ git_up_noai.sh
@@ -24,5 +24,15 @@ echo "-------- END BRANCH[$where ($branch)] ----------";
 git submodule update --progress --init --recursive --force --merge --rebase --remote
 git checkout $branch --
 git pull origin $branch --autostash --recurse-submodules --allow-unrelated-histories --prune --progress -v --rebase
+#old branches
+git push origin --delete cs0.2.03
+git push origin --delete cs0.2.04
+git push origin --delete cs0.2.05
+git push origin --delete cs0.2.06
+git push origin --delete cs0.2.07
+git push origin --delete cs0.2.08
+git push origin --delete cs0.2.09
+git push origin --delete cs0.2.10
+sed -i -e 's/\r$//' "$me"
 echo "-------- END PULL[$where ($branch)] ----------";
 
