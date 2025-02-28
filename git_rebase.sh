7fede6f627634b55c36b0b1bf0582536e5e8c59f
marco76tv <marco.sottana@gmail.com>
2025-02-28 10:23:50 +0100

    .

--- git_rebase.sh
+++ git_rebase.sh
@@ -12,5 +12,6 @@ where=$(pwd)
 echo "N: $count"
 for(( i=1; i<=$count; i++ ))
 do
+find . -type f -name "*:Zone.Identifier" -exec rm -f {} \;
 git add -A && oco --yes && git rebase --continue || git push -uf origin HEAD:$branch && git rebase --continue || git rebase --continue || echo "loop: $i"
 done 
