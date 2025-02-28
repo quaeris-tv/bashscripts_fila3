3f4bf027347c1582f19a48ff7c172176dd90110a
marco76tv <marco.sottana@gmail.com>
2025-02-28 18:03:13 +0100

    chore: remove trailing newlines and add a line to remove Windows-style line endings in git_init.sh docs(prompt.txt): update documentation with correct syntax for fields and labels, improve readability and consistency

--- git_init.sh
+++ git_init.sh
@@ -9,6 +9,7 @@ me=$( readlink -f -- "$0";)
 branch=$1
 where=$(pwd)
 git submodule foreach "$me" "$branch"
+find . -type f -name "*:Zone.Identifier" -exec rm -f {} \;
 git config pull.rebase true
 git config remote.origin.push HEAD
 git config core.autocrlf true
@@ -20,4 +21,5 @@ git config advice.skippedCherryPicks false
 git remote set-branches --add origin $branch
 git push --recurse-submodules=on-demand
 git branch --set-upstream-to=origin/$branch $branch
+sed -i -e 's/\r$//' "$me"
 
