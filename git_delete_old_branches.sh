6ceb10f0ffe8d34a3c4d987cb5569192369b6aa2
marco76tv <marco.sottana@gmail.com>
2025-03-04 17:24:31 +0100

    .

---
+++ git_delete_old_branches.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+git push quaeris-tv --delete 0.0.1
+git push quaeris-tv --delete cs0.2.03
+git push quaeris-tv --delete cs0.2.04
+git push quaeris-tv --delete cs0.2.05
+git push quaeris-tv --delete cs0.2.06
+git push quaeris-tv --delete cs0.2.07
+git push quaeris-tv --delete cs0.2.08
+git push quaeris-tv --delete cs0.2.09
+git push quaeris-tv --delete cs0.2.10
+
+git push quaeris-tv --delete v0.2.06
+git push quaeris-tv --delete v0.2.07
+git push quaeris-tv --delete v0.2.08
+git push quaeris-tv --delete v0.2.09
+
+
+git push origin --delete 0.0.1
+git push origin --delete cs0.2.03
+git push origin --delete cs0.2.04
+git push origin --delete cs0.2.05
+git push origin --delete cs0.2.06
+git push origin --delete cs0.2.07
+git push origin --delete cs0.2.08
+git push origin --delete cs0.2.09
+git push origin --delete cs0.2.10
+
+git push origin --delete v0.2.06
+git push origin --delete v0.2.07
+git push origin --delete v0.2.08
+git push origin --delete v0.2.09
