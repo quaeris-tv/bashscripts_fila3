c8df90f67c2093fe444cc8dd52dadab5afd4bc00
marco76tv <marco.sottana@gmail.com>
2025-03-04 17:58:09 +0100

    .

--- git_delete_old_branches.sh
+++ git_delete_old_branches.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
-git push quaeris-tv --delete 0.0.1
-git push quaeris-tv --delete cs0.2.03
-git push quaeris-tv --delete cs0.2.04
-git push quaeris-tv --delete cs0.2.05
-git push quaeris-tv --delete cs0.2.06
-git push quaeris-tv --delete cs0.2.07
-git push quaeris-tv --delete cs0.2.08
-git push quaeris-tv --delete cs0.2.09
-git push quaeris-tv --delete cs0.2.10
+# Ottieni il percorso assoluto dello script
+me=$(readlink -f -- "$0")
 
-git push quaeris-tv --delete v0.2.06
-git push quaeris-tv --delete v0.2.07
-git push quaeris-tv --delete v0.2.08
-git push quaeris-tv --delete v0.2.09
+# Esegui il comando su tutti i submodule
+git submodule foreach "$me"
 
+# Definisci i branch da eliminare
+branches_to_delete="
+    0.0.1
+    cs0.1.01
+    cs0.2.00
+    cs0.2.01
+    cs0.2.02
+    cs0.2.03
+    cs0.2.04
+    cs0.2.05
+    cs0.2.06
+    cs0.2.07
+    cs0.2.08
+    cs0.2.09
+    cs0.2.10
+    v0.2.06
+    v0.2.07
+    v0.2.08
+    v0.2.09
+    v0.2.10
+    v1.x
+    gh-pages
+    dependabot/github_actions/ramsey/composer-install-3
+    dependabot/npm_and_yarn/postcss-nesting-13.0.0
+"
 
-git push origin --delete 0.0.1
-git push origin --delete cs0.2.03
-git push origin --delete cs0.2.04
-git push origin --delete cs0.2.05
-git push origin --delete cs0.2.06
-git push origin --delete cs0.2.07
-git push origin --delete cs0.2.08
-git push origin --delete cs0.2.09
-git push origin --delete cs0.2.10
-
-git push origin --delete v0.2.06
-git push origin --delete v0.2.07
-git push origin --delete v0.2.08
-git push origin --delete v0.2.09
+# Itera su tutti i remote configurati
+for remote in $(git remote); do
+    for branch in $branches_to_delete; do
+        echo "Deleting branch '$branch' from remote '$remote'..."
+        git push "$remote" --delete "$branch"
+    done
+done
