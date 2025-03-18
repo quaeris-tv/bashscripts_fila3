4c44e3097ca95042cb445a29abdbd0edec65da94
marco76tv <marco.sottana@gmail.com>
2025-03-18 15:34:47 +0100

    up

--- git_up_noai.sh
+++ git_up_noai.sh
@@ -14,13 +14,6 @@ git submodule foreach "$me" "$branch"
 find . -type f -name "*:Zone.Identifier" -exec rm -f {} \;
 #old branches
 #git push origin --delete cs0.2.03
-#git push origin --delete cs0.2.04
-#git push origin --delete cs0.2.05
-#git push origin --delete cs0.2.06
-#git push origin --delete cs0.2.07
-#git push origin --delete cs0.2.08
-#git push origin --delete cs0.2.09
-#git push origin --delete cs0.2.10
 
 git config core.fileMode false
 git config advice.submoduleMergeConflict false
